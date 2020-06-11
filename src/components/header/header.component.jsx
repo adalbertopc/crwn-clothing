e1845a97e4aa7e2094366dfa53cb39dfc001e8f6
adalbertopc <58749813+adalbertopc@users.noreply.github.com>
2020-06-10 23:32:43 -0600

    header agregado

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/crwn-logo.svg';
+
+import './header.styles.scss';
+const Header = (props) => (
+	<div className='header'>
+		<Link to='/' className='logo-container'>
+			<Logo className='logo'></Logo>
+		</Link>
+		<div className='options'>
+			<Link to='/shop' className='option'>
+				SHOP
+			</Link>
+			<Link to='/contact' className='option'>
+				CONTACT
+			</Link>
+		</div>
+	</div>
+);
+
+export default Header;
