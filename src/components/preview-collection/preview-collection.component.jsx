4770440c665fb5087c46fdd2c3e246dcccf3aca4
adalbertopc <58749813+adalbertopc@users.noreply.github.com>
2020-06-10 22:56:57 -0600

    router instalado y creados los componentes collectionPreview y collectionItemÂ

---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+import './preview-collection.styles.scss';
+
+const PreviewCollection = ({ title, items }) => (
+	<div className='preview-collection'>
+		<h1 className='title'>{title.toUpperCase()}</h1>
+		<div className='preview'>
+			{items
+				.filter((item, index) => index < 4)
+				.map(({ id, ...itemProps }) => (
+					<CollectionItem key={id} {...itemProps} />
+				))}
+		</div>
+	</div>
+);
+
+export default PreviewCollection;
